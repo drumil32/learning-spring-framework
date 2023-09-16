ecdf0c2f7505b0ae90e14e4934e15ec3306278e1
drumil <akheniad@gmail.com>
2023-09-16 17:00:22 +0530

    lasy vs eager intialization

---
+++ spring-framework-03/src/main/java/com/learn/springframework01/LazyInitializationLauncherApplication.java
@@ -0,0 +1,46 @@
+package com.learn.springframework01;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Lazy
+class A{
+	A(){
+		System.out.println("ok A is done");
+	}
+	public void helper() {
+		System.out.println("olu olu");
+	}
+}
+
+@Component
+//@Lazy
+class B{
+	private A a;
+	B(A a){
+		System.out.println("here we are");
+		this.a = a;
+	}
+	public void doSomething() {
+		System.out.println("we are here");
+	}
+}
+@Configuration
+@ComponentScan
+public class LazyInitializationLauncherApplication {
+	public static void main(String args[]) {
+		try(var context = new AnnotationConfigApplicationContext(LazyInitializationLauncherApplication.class)){
+			Arrays.stream(context.getBeanDefinitionNames()).forEach(bean->System.out.println(bean));
+			System.out.println("we done");
+			context.getBean(A.class).helper();
+			context.getBean(B.class).doSomething();
+		}
+	}
+}
