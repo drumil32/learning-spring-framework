98c229f444888b2b3f3d961ba6c6088854d17574
drumil <akheniad@gmail.com>
2023-09-17 22:18:51 +0530

    connected to h2 database and created table

---
+++ learn-jpa-and-hibernet/src/test/java/com/springboot/learnjpaandhibernet/LearnJpaAndHibernetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.learnjpaandhibernet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJpaAndHibernetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
