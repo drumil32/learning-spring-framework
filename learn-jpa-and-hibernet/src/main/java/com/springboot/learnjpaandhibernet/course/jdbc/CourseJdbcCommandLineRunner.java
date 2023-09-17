e02ca314fea1afad80470f311f762c98ac946a82
drumil <akheniad@gmail.com>
2023-09-17 23:18:53 +0530

    insert & delete using spring jdbc

--- learn-jpa-and-hibernet/src/main/java/com/springboot/learnjpaandhibernet/course/jdbc/CourseJdbcCommandLineRunner.java
+++ learn-jpa-and-hibernet/src/main/java/com/springboot/learnjpaandhibernet/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.springboot.learnjpaandhibernet.course.Course;
+
 @Component
 public class CourseJdbcCommandLineRunner implements CommandLineRunner{
 	
@@ -12,7 +14,10 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		repository.insert();
+		repository.insert(new Course(1,"Learn AWS","Drumil Akhenia"));
+		repository.insert(new Course(2,"Learn Docker","Drumil Akhenia"));
+		repository.insert(new Course(3,"Learn K8s","Drumil Akhenia"));
+		repository.delete(1);
 	}
 
 }
