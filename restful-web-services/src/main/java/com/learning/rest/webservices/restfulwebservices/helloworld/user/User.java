eb159b83239f159293b6b0fdc9a92c714300ab65
drumil <akheniad@gmail.com>
2023-09-22 22:33:41 +0530

    feat:post entity is created

--- restful-web-services/src/main/java/com/learning/rest/webservices/restfulwebservices/helloworld/user/User.java
+++ restful-web-services/src/main/java/com/learning/rest/webservices/restfulwebservices/helloworld/user/User.java
@@ -1,27 +1,34 @@
 package com.learning.rest.webservices.restfulwebservices.helloworld.user;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
-@Entity(name="user_details")
+@Entity(name = "user_details")
 public class User {
 	@Id
 	@GeneratedValue
 	private Integer id;
-	
-	@Size(min=2,message="name must have atleast 2 charecters")
+
+	@Size(min = 2, message = "name must have atleast 2 charecters")
 	private String name;
-	
-	@Past(message="birthdate should be in the past")
+
+	@Past(message = "birthdate should be in the past")
 	@JsonProperty("birth_date") // if we want to change name of field in side json formate
 	private LocalDate birthDate;
+
+	@OneToMany(mappedBy = "user")
+	@JsonIgnore
+	private List<Post> post;
 
 	public User(int id, String name, LocalDate birthDate) {
 		super();
@@ -29,7 +36,10 @@ public class User {
 		this.birthDate = birthDate;
 		this.name = name;
 	}
-	public User() {}
+
+	public User() {
+	}
+
 	public int getId() {
 		return id;
 	}
---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,23 @@
+INSERT INTO user_details (id,name,birth_date)
+values (1001,'drumil','2002-11-16');
+
+INSERT INTO user_details (id,name,birth_date)
+values (1002,'abc','2003-11-16');
+
+INSERT INTO user_details (id,name,birth_date)
+values (1003,'pqr','2002-11-26');
+
+INSERT INTO user_details (id,name,birth_date)
+values (1004,'xyz','2000-11-16');
+
+INSERT INTO post (id,user_id,description)
+values (101,1001,'docker');
+
+INSERT INTO post (id,user_id,description)
+values (102,1002,'aws');
+
+INSERT INTO post (id,user_id,description)
+values (103,1003,'k8s');
+
+INSERT INTO post (id,user_id,description)
+values (104,1004,'gcp');
