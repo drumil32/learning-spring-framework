528cf8ec7ebbc5150fc6b207549a25f20cc93beb
drumil <akheniad@gmail.com>
2023-09-22 18:34:36 +0530

    request parameter & headers versioning

--- restful-web-services/src/main/java/com/learning/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
+++ restful-web-services/src/main/java/com/learning/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
@@ -21,4 +21,24 @@ public class VersioningPersonController {
 	public PersonV2 getPersonDetailsV2() {
 		return service.getPersonV2();
 	}
+	
+	@GetMapping(path="/person",params = "version=1")
+	public PersonV1 getPersonDetailsBasedOnParamsV1() {
+		return service.getPersonV1();
+	}
+	
+	@GetMapping(path="/person",params = "version=2")
+	public PersonV2 getPersonDetailsBasedOnParamsV2() {
+		return service.getPersonV2();
+	}
+	
+	@GetMapping(path="/person/header",headers = "X-API-VERSION=1")
+	public PersonV1 getPersonDetailsBasedOnHeaderV1() {
+		return service.getPersonV1();
+	}
+	
+	@GetMapping(path="/person/header",headers = "X-API-VERSION=2")
+	public PersonV2 getPersonDetailsBasedOnHeaderV2() {
+		return service.getPersonV2();
+	}
 }
