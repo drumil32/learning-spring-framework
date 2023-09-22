546ea47659d23e863e31479c65da4547d18e3dd7
drumil <akheniad@gmail.com>
2023-09-22 20:52:57 +0530

    dynamic filtering

--- restful-web-services/src/main/java/com/learning/rest/webservices/restfulwebservices/filtering/FilteringController.java
+++ restful-web-services/src/main/java/com/learning/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -2,20 +2,66 @@ package com.learning.rest.webservices.restfulwebservices.filtering;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 @RestController
 public class FilteringController {
-
-	@GetMapping("/users-filtering") 
-	public List<User> retriveAllUsers(){
-		List<User> arr = new ArrayList<>();
+	private static List<User> arr = new ArrayList<>();
+	
+	static {
 		arr.add(new User("drumil","123",1,"a@gm.com"));
 		arr.add(new User("raxit","345",2,"r@gm.com"));
 		arr.add(new User("meet","456",3,"g@gm.com"));
 		arr.add(new User("jay","789",4,"k@gm.com"));
+	}
+
+	@GetMapping("/users-filtering") 
+	public List<User> retriveAllUsers(){
 		return arr;
+	}
+	
+	@GetMapping("/users-filtering/{id}")
+	public User retriveUserById(@PathVariable int id){
+		Predicate<? super User> predicate = user -> user.getId()==id;
+		return arr.stream()
+			.filter(predicate)
+			.findFirst()
+			.orElse(null);
+	}
+	
+	@GetMapping("/users-filtering-dynamic")
+	public MappingJacksonValue retriveAllUsersDynamicFiltering(){
+		List<User> users = arr;
+
+	    MappingJacksonValue mapping = new MappingJacksonValue(users);
+	    SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("id");
+	    FilterProvider filters = new SimpleFilterProvider().addFilter("users-filtering-dynamic-filter", filter);
+	    mapping.setFilters(filters);
+
+	    return mapping;
+	}
+	
+	@GetMapping("/users-filtering-dynamic/{id}")
+	public MappingJacksonValue retriveUserDynamicById(@PathVariable int id){
+		Predicate<? super User> predicate = user -> user.getId()==id;
+		User user = arr.stream()
+			.filter(predicate)
+			.findFirst()
+			.orElse(null);
+	    MappingJacksonValue mapping = new MappingJacksonValue(user);
+	    SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("username","emailId");
+	    FilterProvider filters = new SimpleFilterProvider().addFilter("users-filtering-dynamic-filter", filter);
+	    mapping.setFilters(filters);
+
+	    return mapping;
 	}
 }
--- restful-web-services/src/main/java/com/learning/rest/webservices/restfulwebservices/filtering/User.java
+++ restful-web-services/src/main/java/com/learning/rest/webservices/restfulwebservices/filtering/User.java
@@ -1,13 +1,18 @@
 package com.learning.rest.webservices.restfulwebservices.filtering;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+/*
+NOTE :- we should use field level static filtering because if field name is changed then we need to change it class level static filtering 
+	but in field level static filtering we don't need to change it */
 
-@JsonIgnoreProperties({"emailId","id"}) // class level static filtering
+@JsonIgnoreProperties({"emailId"}) // class level static filtering
+@JsonFilter("users-filtering-dynamic-filter")	// this is for dynamic filtering
 public class User {
 	private String username;
 	
-	@JsonIgnore // filed level static filtering
+//	@JsonIgnore // filed level static filtering
 	private String password;
 	private int id;
 	private String emailId;
