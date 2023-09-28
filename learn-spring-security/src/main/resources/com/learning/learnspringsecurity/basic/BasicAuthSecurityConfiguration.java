cdbc3565a5178d22642291b75aac1de3cf39aa1f
drumil <akheniad@gmail.com>
2023-09-28 19:21:32 +0530

    fix:csrf bug

--- learn-spring-security/src/main/resources/com/learning/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ learn-spring-security/src/main/resources/com/learning/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -3,15 +3,17 @@ package com.learning.learnspringsecurity.basic;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 public class BasicAuthSecurityConfiguration {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
-        http.httpBasic(withDefaults());
+        http.httpBasic(withDefaults()); 
         http.sessionManagement(
             session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.csrf(csrf -> csrf.disable()); // Disable CSRF protection
